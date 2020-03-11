51f146ab85fd75f903b0fc5a370caef18a2b8c82
AZn5ReD <1781837+AZn5ReD@users.noreply.github.com>
2020-03-11 23:57:43 +0000

    ValueHelpDialog almost handled...

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -8,21 +8,35 @@ sap.ui.define([
 	return Controller.extend("ui5.util.ui5-util.controller.View", {
 		onInit: function () {
 			var validator = new Validator();
-			
+
 			this.getView().getModel().read("/Products", {
 				success: function (oData) {
 					console.log("Connected to Northwind");
 					console.log("Products:", oData);
-				},
+					this.getView().setModel(new sap.ui.model.json.JSONModel(oData), "Products");
+				}.bind(this),
 				error: function (oError) {
 					console.log("Not connected to Northwind")
 					console.log(oError);
 				}
-			})
+			});
+
+			this.getView().setModel(new sap.ui.model.json.JSONModel(), "InputModel");
 		},
-		
+
 		onValueHelpDialog: function (oEvent) {
-			ValueHelpDialog.handleValueHelpDialog(this, oEvent);
+			ValueHelpDialog.handleValueHelpDialog(this, oEvent, {
+				title: "Products",
+				key: "ID",
+				description: "Name",
+				cols: [{
+					label: "ID",
+					template: "ID"
+				}, {
+					label: "Name",
+					template: "Name"
+				}, ],
+			});
 		}
 	});
 });
--- webapp/ui5-util/ValueHelpDialog.js
+++ webapp/ui5-util/ValueHelpDialog.js
@@ -3,21 +3,22 @@ sap.ui.define([
 	"sap/ui/comp/filterbar/FilterBar",
 	"sap/ui/comp/filterbar/FilterGroupItem",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/Filter"
-], function (ValueHelpDialog, FilterBar, FilterGroupItem, JSONModel, Filter) {
+	"sap/ui/model/Filter",
+	"sap/m/Input"
+], function (ValueHelpDialog, FilterBar, FilterGroupItem, JSONModel, Filter, Input) {
 	"use strict";
 
 	return {
-		handleValueHelpDialog: function (oContext, oEvent) {
-			//TODO Add Object Param model
-			//TODO Add Function model
-			var that = oContext;
-			//var oContext = oEvent.getSource().getParent().getParent().getBindingContext("detailData");
+		handleValueHelpDialog: function (oThis, oEvent, oParams, oFunctions) {
+			// Get context if needed (in a table for example)
+			var oContext = oEvent.getSource().getParent().getParent().getBindingContext(oParams.model) || null;
 
-			var fnOk = function (oOkEvent) {
-				var oToken = oEvent.getParameter("tokens")[0];
-				that.getModel("detailData").setProperty("WBSElementExt", oToken.data("row").WBSExt, oContext);
-				that.getModel("detailData").setProperty("WBSElementDescr", oToken.data("row").WBSDescr, oContext);
+			// Define default functions
+			var fnOk = function (oEvent) {
+				var oToken = oEvent.getParameter("tokens")[0],
+					oValueHelpSource = oEvent.getSource().data("valueHelpSource");
+				oThis.getView().getModel(oParams.model).setProperty(oParams.key, oToken.data("row")[oParams.key], oContext);
+				oThis.getView().getModel(oParams.model).setProperty(oParams.description, oToken.data("row")[oParams.description], oContext);
 				this.close();
 			};
 
@@ -51,63 +52,68 @@ sap.ui.define([
 				oValueHelpDialog.update();
 			};
 
+			// Replace with custom functions
+			if (oFunctions) {
+				fnOk = oFunctions.ok || fnOK;
+				fnCancel = oFunctions.cancel || fnCancel;
+				fnAfterClose = oFunctions.afterClose || fnAfterClose;
+				fnSearch = oFunctions.search || fnSearch;
+			}
+
+			// Define value help dialog
 			var oValueHelpDialog = new ValueHelpDialog({
-				title: this.getResourceBundle().getText("itemDetailAccountAssignmentProjectLabel"),
-				ok: fnOK,
-				cancel: fnCancel,
-				afterClose: fnAfterClose,
+				title: oParams.title,
 				supportMultiselect: false,
-				key: "WBSExt",
-				descriptionKey: "WBSDescr",
+				key: oParams.key,
+				descriptionKey: oParams.description,
 				filterBar: new FilterBar({
 					filterGroupItems: [
 						new FilterGroupItem({
-							name: "WBSExt",
+							name: oParams.key,
 							groupName: "__$INTERNAL$",
-							label: this.getResourceBundle().getText("itemDetailAccountAssignmentProjectLabel"),
+							label: oParams.cols[0].label, // 1st column label
 							visibleInFilterBar: true,
 							control: new Input({
-								name: "WBSExt"
+								name: oParams.key
 							})
 						}),
 						new FilterGroupItem({
-							name: "WBSDescr",
+							name: oParams.description,
 							groupName: "__$INTERNAL$",
-							label: this.getResourceBundle().getText("detailTableColumnDescription"),
+							label: oParams.cols[1].label, // 2nd column label
 							visibleInFilterBar: true,
 							control: new Input({
-								name: "WBSDescr"
+								name: oParams.key
 							})
 						})
 					],
 					search: fnSearch
-				})
+				}),
+				customData: [{
+					key: "valueHelpSource",
+					value: oEvent.getSource()			// Keep reference of source
+				}],
+				ok: fnOk,
+				cancel: fnCancel,
+				afterClose: fnAfterClose
 			});
 
+			// Set value help dialog models
 			var oTable = oValueHelpDialog.getTable();
 			oTable.setModel(new JSONModel({
-				"cols": [{
-					"label": this.getResourceBundle().getText("itemDetailAccountAssignmentProjectLabel"),
-					"template": "WBSExt"
-				}, {
-					"label": this.getResourceBundle().getText("detailTableColumnDescription"),
-					"template": "WBSDescr"
-				}]
+				cols: oParams.cols
 			}), "columns");
+			
 			if (oTable.bindRows) {
-				var sCompanyCode = this.getModel("detailData").getProperty("/Company/CompanyCode");
+				// TODO Handle oDataModel and JSONModel
+				// TODO Handle custom binding ? (filters, etc.)
 				//        oTable.setModel(this.getModel("ProjectSet"), "ProjectSet");
-				oTable.setModel(that.getModel());
+				oTable.setModel(oThis.getView().getModel());
 				oTable.bindRows({
 					path: "/Products",
-					// filters: [new Filter({
-					// 	path: "CompanyCode",
-					// 	operator: "EQ",
-					// 	value1: sCompanyCode
-					// })]
 				});
 			}
-			
+
 			oValueHelpDialog.open();
 		}
 	};
